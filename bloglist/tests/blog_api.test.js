67af2d8b5dd31a5fe5d0a27e9d71cdf19a78c575
Sivasurya Sundaram <gamerganapathi@live.com>
2023-12-10 22:30:31 +0530

    exercise 4.23

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -2,11 +2,49 @@ const supertest = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../app');
 const Blog = require('../models/blogs');
+const User = require('../models/users');
 const testHelper = require('./test_helper');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const api = supertest(app);
+var adToken;
 beforeEach(async () => {
+  await User.deleteMany({});
+  const passwordHash = await bcrypt.hash('sekert', 10);
+  const user = new User({
+    username: 'root',
+    name: 'root',
+    passwordHash: passwordHash,
+  });
+  await user.save();
+  let userInDb = await User.findOne({ username: 'root' });
+  const userToken = {
+    username: userInDb.username,
+    id: userInDb._id,
+  };
+  const initialBlogs = [
+    {
+      title: 'blog1',
+      author: 'sivasurya',
+      url: 'www.blog1.com',
+      likes: 2,
+      user: userInDb._id,
+    },
+    {
+      title: 'blog2',
+      author: 'anu',
+      url: 'www.blog2.com',
+      likes: 5,
+      user: userInDb._id,
+    },
+  ];
+  adToken = jwt.sign(userToken, process.env.SECRET, { expiresIn: 60 * 60 });
   await Blog.deleteMany({});
-  await Blog.insertMany(testHelper.initialBlogs);
+  await Blog.insertMany(initialBlogs);
+  userInDb = await User.findOne({ username: 'root' });
+  const blogsInDb = await (await Blog.find({})).map((x) => x._id);
+  userInDb.blogs = userInDb.blogs.concat(blogsInDb);
+  await userInDb.save();
 });
 describe('when there is data in database', () => {
   test('blogs are returned as JSON', async () => {
@@ -33,7 +71,7 @@ describe('finding a specific blog', () => {
     const foundBlog = await api
       .get(`/api/blogs/${blogToBeViewed.id}`)
       .expect(200);
-    expect(foundBlog.body).toEqual(blogToBeViewed);
+    expect(foundBlog.body.id).toEqual(blogToBeViewed.id);
   });
 });
 describe('adding a new blog', () => {
@@ -46,6 +84,7 @@ describe('adding a new blog', () => {
     };
     await api
       .post('/api/blogs')
+      .set('authorization', `Bearer ${adToken}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -62,6 +101,7 @@ describe('adding a new blog', () => {
     };
     await api
       .post('/api/blogs')
+      .set('authorization', `Bearer ${adToken}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
@@ -78,6 +118,7 @@ describe('adding a new blog', () => {
     };
     await api
       .post('/api/blogs')
+      .set('authorization', `Bearer ${adToken}`)
       .send(newBlog)
       .expect(400)
       .expect('Content-Type', /application\/json/);
@@ -89,6 +130,7 @@ describe('adding a new blog', () => {
     };
     await api
       .post('/api/blogs')
+      .set('authorization', `Bearer ${adToken}`)
       .send(newBlog)
       .expect(400)
       .expect('Content-Type', /application\/json/);
@@ -98,7 +140,10 @@ describe('deletion of specific notes', () => {
   test('deletion of blog succeeds with status code 204', async () => {
     const blogsAtStart = await testHelper.blogsInDb();
     const blogToBeDeleted = blogsAtStart[0];
-    await api.delete(`/api/blogs/${blogToBeDeleted.id}`).expect(204);
+    await api
+      .delete(`/api/blogs/${blogToBeDeleted.id}`)
+      .set('authorization', `Bearer ${adToken}`)
+      .expect(204);
     const blogsAtEnd = await testHelper.blogsInDb();
     expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1);
     const blogTitles = blogsAtEnd.map((blog) => blog.title);
